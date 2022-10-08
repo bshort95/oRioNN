088c3f21e0c6f5ae054b039632ab8a47a1adcef4
colh99 <colbyhale199@gmail.com>
2022-10-08 14:30:49 -0600

    mastermind changes, overall reorganization of files and folders

---
+++ functions/generate_businesses.py
@@ -0,0 +1,33 @@
+import random
+
+def generate_businesses(quantity):
+    '''Returns a list with the given quantity of business names.'''
+    businesses = []
+    # Open each file and store a list of each one
+    with open('data/business_adjectives.txt') as file:
+        adjectives = file.readlines()
+    with open('data/last_names.txt') as file:
+        last_names = file.readlines()
+    with open('data/business_verbs.txt') as file:
+        verbs = file.readlines()
+
+    for n in range(1, quantity + 1):
+        # Get first part, with a chance to use a last name as an adjective instead
+        random_number = random.randint(1, 4)
+        if random_number > 1:
+            adjective = adjectives[random.randint(1, (len(adjectives) - 1))].strip()
+        else:
+            adjective = last_names[random.randint(1, (len(adjectives) - 1))].strip()
+
+        # Get second part
+        verb = verbs[random.randint(1, (len(verbs) - 1))].strip()
+        # Put the parts together and append to list
+        business = adjective + " " + verb
+        businesses.append(business)
+    
+    return businesses
+
+
+# Testing
+# businesses = generate_businesses(10)
+# print(businesses)
--- mastermind.py
+++ mastermind.py
@@ -1,7 +1,7 @@
-from csv_writer import csv_writer
-from generate_businesses import generate_businesses
-from generate_names import generate_names
-from generate_numbers import generate_numbers
+from functions.csv_writer import csv_writer
+from functions.generate_businesses import generate_businesses
+from functions.generate_names import generate_names
+from functions.generate_numbers import generate_numbers
 
 # Get the number of positive survey responses to generate
 valid_input = False
@@ -21,16 +21,21 @@ while valid_input == False:
     else:
         print("Please enter a whole number of 0 or more.")
 
+numbers_list = []
+names_list = []
+business_list = []
+sentences_list = []
+
 if num_positives > 0:
-    names_list = generate_names(num_positives)
-    business_list = generate_businesses(num_positives)
-    numbers_list = generate_numbers(True)
+    numbers_list.append(generate_numbers(True))
+    names_list.append(generate_names(num_positives))
+    business_list.append(generate_businesses(num_positives))
     #sentences_list = model_generator(num_positives)       <---- do this
 
 if num_negatives > 0:
+    numbers_list.append(generate_numbers(True))
     names_list.append(generate_names(num_negatives))
     business_list.append(generate_businesses(num_negatives))
-    numbers_list.append(generate_numbers(True))
     #sentences_list = model_generator(num_positives)       <---- do this
 
 full_list = [names_list, business_list, numbers_list]
