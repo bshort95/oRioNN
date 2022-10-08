30c0ac038b6b7521dd3f1983f76bda6bb2a3108a
Brandon Short <71357028+bshort95@users.noreply.github.com>
2022-10-08 14:46:54 -0600

    Update model_generator.py

--- functions/model_generator.py
+++ functions/model_generator.py
@@ -1,1 +1,23 @@
-import tensorflow as tf
+import random
+
+def generate_reviews(p,n):
+    
+    total_list = []
+    p_list = []
+    n_list = []
+    
+    for x in open("good.txt"):
+        p_list.append(x)
+    for y in open("bad.txt"):
+        n_list.append(y)
+    
+   
+    for i in range(p):
+        total_list.append(n_list[random.randint(0,p)])
+    
+    for j in range(n):
+        total_list.append(n_list[random.randint(0,n)])
+
+    return total_list
+
+print(generate_reviews(1,2))
